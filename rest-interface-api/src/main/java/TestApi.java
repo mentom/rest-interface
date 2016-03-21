517f523faf82354f9ade24d40b0de562592611a2
mentom <tommymenca@gmail.com>
2016-03-21 23:46:21 +0100

    Impostazione features

---
+++ rest-interface-api/src/main/java/TestApi.java
@@ -0,0 +1,9 @@
+/**
+ * Created by mentom on 21/03/16.
+ */
+public class TestApi {
+    public static void saluta(){
+        System.out.println("CIAO");
+    }
+
+}
---
+++ rest-interface-impl/src/main/java/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created by mentom on 21/03/16.
+ */
+public class Main {
+
+    public void main(String args [])
+    {
+        System.out.println("Ciao");
+        TestApi testApi = new TestApi();
+        testApi.saluta();
+
+    }
+
+}
